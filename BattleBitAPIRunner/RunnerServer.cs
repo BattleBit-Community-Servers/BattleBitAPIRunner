2e828186e2670807c06256cbd31df2ce7be6e324
RainOrigami <rain+github@codefreak.net>
2023-08-14 21:39:45 +0200

    Fixes method invocations

--- BattleBitAPIRunner/RunnerServer.cs
+++ BattleBitAPIRunner/RunnerServer.cs
@@ -1,5 +1,6 @@
 ﻿using BattleBitAPI.Common;
 using BattleBitAPI.Server;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Diagnostics;
 using System.Reflection;
 
@@ -34,26 +35,50 @@ namespace BattleBitAPIRunner
             return this.modules.FirstOrDefault(m => m.GetType() == typeof(T)) as T;
         }
 
-        private async Task<object?> invokeOnModulesWithReturnValue(string method, params object?[] parameters)
+        // TODO: this is fucked up and needs to be generalized
+        private async Task<bool> invokeOnModulesWithBoolReturnValue(string method, params object?[] parameters)
         {
             Stopwatch stopwatch = new();
-            object? result = null;
+            bool result = true;
+
             foreach (BattleBitModule module in this.modules)
             {
                 try
                 {
                     stopwatch.Start();
-                    object? moduleResult = await (Task<object?>)typeof(BattleBitModule).GetMethod(method).Invoke(module, parameters);
+                    bool moduleResult = await (Task<bool>)typeof(BattleBitModule).GetMethod(method).Invoke(module, parameters);
 
-                    if (moduleResult is bool boolResult && !boolResult)
+                    if (!moduleResult)
                     {
                         result = false;
                     }
-                    else
-                    {
-                        // TODO: I hate this.
-                        result = moduleResult;
-                    }
+                }
+                catch (Exception ex)
+                {
+                    await Console.Out.WriteLineAsync($"Method {method} on module {module.GetType().Name} threw an exception: {ex.Message}");
+                }
+                stopwatch.Stop();
+
+                if (stopwatch.ElapsedMilliseconds > 250)
+                {
+                    // TODO: move this to a configurable field in ServerConfiguration
+                    await Console.Out.WriteLineAsync($"Method {method} on module {module.GetType().Name} took {stopwatch.ElapsedMilliseconds}ms to execute.");
+                }
+            }
+            return result;
+        }
+
+        private async Task<OnPlayerSpawnArguments> invokeOnModulesWithPlayerSpawnArgumentsReturnValue(string method, params object?[] parameters)
+        {
+            Stopwatch stopwatch = new();
+            OnPlayerSpawnArguments result = default;
+
+            foreach (BattleBitModule module in this.modules)
+            {
+                try
+                {
+                    stopwatch.Start();
+                    result = await (Task<OnPlayerSpawnArguments>)typeof(BattleBitModule).GetMethod(method).Invoke(module, parameters);
                 }
                 catch (Exception ex)
                 {
@@ -97,104 +122,104 @@ namespace BattleBitAPIRunner
         // TODO: there must be a better way to do this!?
         public override async Task OnConnected()
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name);
+            await this.invokeOnModules(nameof(OnConnected));
         }
         public override async Task OnTick()
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name);
+            await this.invokeOnModules(nameof(OnTick));
         }
         public override async Task OnReconnected()
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name);
+            await this.invokeOnModules(nameof(OnReconnected));
         }
         public override async Task OnDisconnected()
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name);
+            await this.invokeOnModules(nameof(OnDisconnected));
         }
         public override async Task OnPlayerConnected(RunnerPlayer player)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, player);
+            await this.invokeOnModules(nameof(OnPlayerConnected), player);
         }
         public override async Task OnPlayerDisconnected(RunnerPlayer player)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, player);
+            await this.invokeOnModules(nameof(OnPlayerDisconnected), player);
         }
         public override async Task<bool> OnPlayerTypedMessage(RunnerPlayer player, ChatChannel channel, string msg)
         {
-            return (bool)await this.invokeOnModulesWithReturnValue(MethodBase.GetCurrentMethod().Name, player, channel, msg);
+            return (bool)await this.invokeOnModulesWithBoolReturnValue(nameof(OnPlayerTypedMessage), player, channel, msg);
         }
         public override async Task OnPlayerJoiningToServer(ulong steamID, PlayerJoiningArguments args)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, steamID, args);
+            await this.invokeOnModules(nameof(OnPlayerJoiningToServer), steamID, args);
         }
         public override async Task OnSavePlayerStats(ulong steamID, PlayerStats stats)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, steamID, stats);
+            await this.invokeOnModules(nameof(OnSavePlayerStats), steamID, stats);
         }
         public override async Task<bool> OnPlayerRequestingToChangeRole(RunnerPlayer player, GameRole requestedRole)
         {
-            return (bool)await this.invokeOnModulesWithReturnValue(MethodBase.GetCurrentMethod().Name, player, requestedRole);
+            return (bool)await this.invokeOnModulesWithBoolReturnValue(nameof(OnPlayerRequestingToChangeRole), player, requestedRole);
         }
         public override async Task<bool> OnPlayerRequestingToChangeTeam(RunnerPlayer player, Team requestedTeam)
         {
-            return (bool)await this.invokeOnModulesWithReturnValue(MethodBase.GetCurrentMethod().Name, player, requestedTeam);
+            return (bool)await this.invokeOnModulesWithBoolReturnValue(nameof(OnPlayerRequestingToChangeTeam), player, requestedTeam);
         }
         public override async Task OnPlayerChangedRole(RunnerPlayer player, GameRole role)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, player, role);
+            await this.invokeOnModules(nameof(OnPlayerChangedRole), player, role);
         }
         public override async Task OnPlayerJoinedSquad(RunnerPlayer player, Squads squad)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, player, squad);
+            await this.invokeOnModules(nameof(OnPlayerJoinedSquad), player, squad);
         }
         public override async Task OnPlayerLeftSquad(RunnerPlayer player, Squads squad)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, player, squad);
+            await this.invokeOnModules(nameof(OnPlayerLeftSquad), player, squad);
         }
         public override async Task OnPlayerChangeTeam(RunnerPlayer player, Team team)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, player, team);
+            await this.invokeOnModules(nameof(OnPlayerChangeTeam), player, team);
         }
         public override async Task<OnPlayerSpawnArguments> OnPlayerSpawning(RunnerPlayer player, OnPlayerSpawnArguments request)
         {
-            return (OnPlayerSpawnArguments)await this.invokeOnModulesWithReturnValue(MethodBase.GetCurrentMethod().Name, player, request);
+            return (OnPlayerSpawnArguments)await this.invokeOnModulesWithPlayerSpawnArgumentsReturnValue(nameof(OnPlayerSpawning), player, request);
         }
         public override async Task OnPlayerSpawned(RunnerPlayer player)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, player);
+            await this.invokeOnModules(nameof(OnPlayerSpawned), player);
         }
         public override async Task OnPlayerDied(RunnerPlayer player)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, player);
+            await this.invokeOnModules(nameof(OnPlayerDied), player);
         }
         public override async Task OnPlayerGivenUp(RunnerPlayer player)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, player);
+            await this.invokeOnModules(nameof(OnPlayerGivenUp), player);
         }
         public override async Task OnAPlayerDownedAnotherPlayer(OnPlayerKillArguments<RunnerPlayer> args)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, args);
+            await this.invokeOnModules(nameof(OnAPlayerDownedAnotherPlayer), args);
         }
         public override async Task OnAPlayerRevivedAnotherPlayer(RunnerPlayer from, RunnerPlayer to)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, from, to);
+            await this.invokeOnModules(nameof(OnAPlayerRevivedAnotherPlayer), from, to);
         }
         public override async Task OnPlayerReported(RunnerPlayer from, RunnerPlayer to, ReportReason reason, string additional)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, from, to, reason, additional);
+            await this.invokeOnModules(nameof(OnPlayerReported), from, to, reason, additional);
         }
         public override async Task OnGameStateChanged(GameState oldState, GameState newState)
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name, oldState, newState);
+            await this.invokeOnModules(nameof(OnGameStateChanged), oldState, newState);
         }
         public override async Task OnRoundStarted()
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name);
+            await this.invokeOnModules(nameof(OnRoundStarted));
         }
 
         public override async Task OnRoundEnded()
         {
-            await this.invokeOnModules(MethodBase.GetCurrentMethod().Name);
+            await this.invokeOnModules(nameof(OnRoundEnded));
         }
     }
 }
